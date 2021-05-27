d2f48129a756423fb5c0dcb9d8eba76834029339
hajra <hajra@dynamitsolutions.com>
2021-05-27 01:17:24 -0700

    Adding libman to use client-side library

---
+++ CorePRactice1/CorePRactice1/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using CorePRactice1.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorePRactice1.Models;
+namespace CorePRactice1.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookrepository = null;
+        public BookController()
+        {
+            _bookrepository = new BookRepository();
+        
+        }
+        public List<bookModel> GetAllBooks()
+        {
+
+            return _bookrepository.getAllbooks();
+        } 
+        public bookModel GetBooksById(int id)
+        {
+
+            return _bookrepository.getBooksByid(id);
+        }
+        public List<bookModel> SearchBooks(string title,string AuthorName)
+        {
+
+            return _bookrepository.SearchBook(title, AuthorName);
+        }
+    }
+}
---
+++ CorePRactice1/CorePRactice1/Repository/BookRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorePRactice1.Models;
+namespace CorePRactice1.Repository
+{
+    public class BookRepository
+    {
+
+        public List<bookModel> getAllbooks()
+        {
+
+           return Datasource();
+        }
+        public bookModel getBooksByid(int id)
+        {
+            return Datasource().Where(x=>x.id==id).FirstOrDefault();
+
+
+        }
+        public List<bookModel> SearchBook(string title,string authorName)
+        {
+            return Datasource().Where(x=>x.Name.Contains(title)&& x.Author.Contains(authorName)).ToList();
+
+
+        }
+        private List<bookModel> Datasource()
+        {
+            return new List<bookModel>()
+            {
+                new bookModel(){id=1,Name="java",Author="abc" },
+                new bookModel(){id=2,Name="C#",Author="abc2" },
+                new bookModel(){id=3,Name="Python",Author="abc3" },
+                new bookModel(){id=5,Name="Javascript",Author="abc5" },
+                new bookModel(){id=4,Name="C++",Author="abc4" },
+
+            };
+        
+        }
+
+    }
+}
